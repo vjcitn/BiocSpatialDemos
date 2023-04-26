23e697420b90a3626669ccb701a87efbb2c56cd4
vjcitn <stvjc@channing.harvard.edu>
2023-04-25 23:04:46 -0400

    first draft

---
+++ R/app.R
@@ -0,0 +1,132 @@
+
+
+cl_phv = c("entire_cell_cd19_opal_480_min", "entire_cell_cd19_opal_480_max", 
+"entire_cell_cd19_opal_480_std_dev", "entire_cell_cd19_opal_480_total", 
+"entire_cell_dapi_dapi_min", "entire_cell_dapi_dapi_max", "entire_cell_dapi_dapi_std_dev", 
+"entire_cell_dapi_dapi_total", "entire_cell_autofluorescence_min", 
+"entire_cell_autofluorescence_max", "entire_cell_autofluorescence_std_dev", 
+"entire_cell_autofluorescence_total")
+
+#' return a vector of colData labels denoting cell phenotypes for EH7312
+#' @export
+ovVP_phv = function() c("phenotype_cd68", "phenotype_ki67", 
+"phenotype_ck", "phenotype_cd19", "phenotype_p_stat3", "phenotype_cd3", 
+"phenotype_cd8")
+
+#' return a vector of colData labels denoting cell phenotypes for EH7311
+#' @export
+luV3_phv = c("phenotype_ck", "phenotype_cd8", "phenotype_cd14", "phenotype_other", 
+"phenotype_cd19", "phenotype_cd4")
+
+
+add_multicol = function(spe, inds=188:194, opts = c("CD68", "CK", "CD19", "CD3", "CD8")) {
+ mymat = data.matrix(colData(spe)[,inds])
+ nopts = paste0(opts, rep(c("+", "-"), each=5))
+ nina = function(x) if (length(x)==0) NA else x
+ zz = apply(mymat, 1, function(x) last(match(nina(intersect(x, nopts[1:5])), nopts[1:5], nomatch=NA)))
+ spe$multicol = factor(opts[zz])
+ spe
+}
+
+show_core = function(spe, colvar = "phenotype_cd8", ...) {
+  xy = spatialCoords(spe)
+  datf = data.frame(x=xy[,1], y=xy[,2])
+  datf$feat = factor(colData(spe)[, colvar])
+  ggplot(datf, aes(x=x, y=y, colour=feat)) + geom_point(...) + labs(title=spe$sample_id[1])
+}
+ 
+
+extract_core = function(id, spe) {
+ stopifnot("nid" %in% names(colData(spe)))
+ spe[, which(spe$nid==id)]
+}
+
+colNplus = function(datf) {
+ ans = do.call(cbind, lapply(datf, function(x) length(grep("\\+$", x))))
+ names(ans) = names(datf)
+ ans
+}
+
+
+get_summaries = function(spe, phv) {
+  dat = as.data.frame(colData(spe)[, c("nid", phv) ])
+  sdat = split(dat, dat$nid)
+  ans = data.frame(do.call(rbind, lapply(sdat, colNplus)))
+  ans$nid = names(sdat)
+  ans
+}
+
+#' Explore ovVP Vectra Polaris data from ExperimentHub EH7312
+#' @import SpatialExperiment
+#' @import ggplot2
+#' @rawNamespace import("shiny", except=c("renderDataTable", "dataTableOutput"))
+#' @import DT
+#' @import dplyr
+#' @param spe the SpatialExperiment from EH7312
+#' @param phv character() vector of phenotype labels, from colData
+#' @param add_cd4 logical(1) if TRUE, use CD3+ CD8- to enumerate CD4
+#' @param add_multicol logical(1) if TRUE, add a multilevel factor for cell type
+#' @note  App permits selection of specific tissue slices on tissue microarray, coloring
+#' the points at cell coordinates in `spatialCoords`.  The input SpatialExperiment must
+#' include a column 'nid' of numerical slice ids in colData.
+#' @examples
+#' if (interactive()) {
+#'   requireNamespace("ExperimentHub")
+#'   ovVP = ExperimentHub::ExperimentHub()[["EH7312"]]
+#'   ovVP$nid = as.numeric(factor(ovVP$sample_id))
+#'   display_by_core(ovVP, ovVP_phv())
+#' }
+#' @export
+display_by_core = function(spe, phv, add_cd4=TRUE, add_multicol=TRUE) {
+ stopifnot("nid" %in% names(colData(spe)))
+ spe$cd4 = "CD4-"
+ if (add_cd4) {
+   kp = which(spe$phenotype_cd3=="CD3+")
+   spe$cd4[kp] = ifelse(spe$phenotype_cd8[kp]=="CD8-", "CD4+", "CD4-")
+   phv = c(phv, "cd4")
+   }
+ if (add_multicol) phv = c("multicol", phv)
+ ui = fluidPage(
+  sidebarLayout(
+   sidebarPanel(
+    selectInput("core", "core", choices=sort(unique(spe$nid))),
+    radioButtons("phenotype", "phenotype", choices=phv),
+    actionButton("stopBtn", "Stop app"), width=2
+    ),
+   mainPanel(
+    tabsetPanel(
+     tabPanel("core", plotOutput("coreview", height="550px")),
+     tabPanel("phdata", DT::dataTableOutput("datatab")),
+     tabPanel("about", helpText("Data described in PMID 34615692.  We use CD3+ and CD8- to label cells CD4+; all others are labeled CD4-."))
+     )
+    )
+   )
+  )
+ server = function(input, output) {
+  output$coreview = renderPlot({
+    newov = extract_core( input$core, spe )
+    tmp = newov
+    save(tmp, file="tmp.rda")
+    if (add_multicol) {
+      newov = add_multicol(newov)
+      }
+    show_core(newov, input$phenotype)
+    })
+  output$datatab = DT::renderDataTable({
+    if (!exists("ovVP_summaries")) data("ovVP_summaries", package="BiocSpatialDemos")
+    ovVP_summaries
+    })
+  observe({
+      if (input$stopBtn > 0) {
+        isolate({
+          stopApp(returnValue = 0)
+        })
+      }
+    })
+
+ }
+ runApp(list(ui=ui, server=server))
+}
+   
+   
+    
---
+++ R/data.R
@@ -0,0 +1,5 @@
+#' table of sums of phenotype counts collapsed over cells within tissue microarray slices
+#' @docType data
+#' @format data.frame
+#' @note Produced using get_summaries
+"ovVP_summaries"
